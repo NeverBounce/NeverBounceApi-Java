82d1fbcc4f94131c2ef4b13ac53ba4764f6ae336
Laszlo Csontos <github@laszlocsontos.com>
2017-07-31 19:52:05 +0200

    #8 Adjust script for regenerating javadoc

--- regenerate-javadoc.sh
+++ regenerate-javadoc.sh
@@ -2,14 +2,23 @@
 
 ./mvnw -pl neverbounce-api-client clean javadoc:javadoc
 
-APIDOCS="neverbounce-api-client/target/site/apidocs"
-[ -d $APIDOCS ] && cd $APIDOCS || exit 1
+OLD_PWD=$(pwd)
+API_DOCS="${OLD_PWD}/neverbounce-api-client/target/site/apidocs"
+[ -d ${API_DOCS} ] && cd ${API_DOCS} || exit 1
+
+TMP_API_DOCS=$(mktemp -d)
+cd ${TMP_API_DOCS}
 
 git init
-git remote add javadoc git@github.com:NeverBounce/NeverBounceApi-Java.git
+git remote add upstream git@github.com:NeverBounce/NeverBounceApi-Java.git
+git remote add origin git@github.com:laszlocsontosuw/NeverBounceApi-Java.git
 
+git fetch upstream gh-pages:gh-pages
+git checkout gh-pages
+
+cp -R ${API_DOCS}/* ${TMP_API_DOCS}
 git add --all
 git commit -m "Regenerated Javadoc"
-git push javadoc master:gh-pages -f
+git push origin gh-pages:gh-pages -f
 
-cd ../../../..
+cd $OLD_PWD
